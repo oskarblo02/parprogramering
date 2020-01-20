b6443ad75c56a64f876523100d302ee700292029
oskarblo02 <svartaormen@gmail.com>
2020-01-20 11:01:36 +0100

    nemo

---
+++ nemo.py
@@ -0,0 +1,7 @@
+def find_nemo(sentence):
+    sentence = input('skriv ')
+	if 'nemo' in sentence:
+        return('vi hittade nemo')
+	elif nemo not in sentence:
+		return('vi hittade inte nemo')
+    
